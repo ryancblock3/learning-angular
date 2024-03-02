6041815c053d1c80526497d89d96e31be1d26da1
ryan <155644277+rbloc13@users.noreply.github.com>
2024-03-02 11:58:22 -0500

    Was in the wrong directory, now added everything

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrl: './server.component.css',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
+})
+export class ServerComponent {
+  serverId = 333;
+  serverStatus = 'Offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
+  }
+}
